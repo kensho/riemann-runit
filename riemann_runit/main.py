3bc46968ad4ec34d98ec33d10c12dec72ba141b1
Ashok Rao <@kensho.com>
2015-05-29 15:56:59 -0400

    code running with riemann, added functionality

--- riemann_runit/main.py
+++ riemann_runit/main.py
@@ -2,33 +2,39 @@ from collections import defaultdict
 import threading
 import subprocess
 import socket
-
+import glob
+import shlex
 
 import click
+from bernhard import Client
 
 class ParseToRiemann(object):
 
 	def __init__(self, timeout, interval, procs, directory, host, port, limit):
 		self.timeout = timeout
-		self.interval = interval
-		self.procs = procs.split(',')
+		self.interval = int(interval)
+		self.procs = str(procs).split(',')
 		self.directory = directory
 		self.host = host
 		self.port = port
 		self.limit = limit
 		self.syshost = socket.gethostname()
+		self.client = Client(self.host, int(self.port))
 
-		self.metric = self.parse(self.procs, self.directory)
+		self.metric = defaultdict(lambda : 0)
 		self.status = dict()
 		self.data   = defaultdict(list) 
 		self.max_time_running = defaultdict(lambda: 0)
 
 	def grab(self):
 		if not self.directory.endswith('/*'):
-			self.directory.append('/*')
-		args = ['sv', 'status', self.directory]
+			self.directory += '*'
 
-		return subprocess.check_output(args).split('\n')
+		cmd = 'sv status ' + self.directory	
+		arg = shlex.split(cmd)
+		arg = arg[:-1] + glob.glob(arg[-1])
+
+		return subprocess.check_output(arg).split('\n')
 
 	def parse_and_update(self):
 		output = self.grab()
@@ -40,9 +46,8 @@ class ParseToRiemann(object):
 
 			service_name = split_by_space[1].split('/')[-1]
 			time_running = int(split_by_space[-1].replace('s', ''))
-
-			if service_name in self.procs or len(self.procs) == 0:				
-				self.data[service_name].append(time_running)
+			if service_name in self.procs or len(self.procs) <= 1:	
+				self.data[service_name].append(int(time_running))
 
 				if time_running > self.max_time_running[service_name]:
 					self.max_time_running[service_name] = time_running
@@ -51,7 +56,7 @@ class ParseToRiemann(object):
 					self.data[service_name].pop()
 
 		new_times_running = dict()
-		for k, v in self.data:
+		for k, v in self.data.iteritems():
 			new_times_running[k] = v[-1]
 
 		return new_times_running
@@ -60,29 +65,28 @@ class ParseToRiemann(object):
 		status = dict()
 		for k, v in self.data.iteritems():
 			if len(self.data[k]) > 1:
-				status[k] = self.data[k][-1] - self.data[k][-2] > self.interval + 1
+				status[k] = str(self.data[k][-1] - self.data[k][-2] > self.interval - 1)
 			else:
-				status[k] = True
+				status[k] = str(True)
+
+		return status
 			
 	def collect_and_emit(self):
 		new_times_running = self.parse_and_update()
 		self.status = self.alive_or_dead()
 
-		from riemann_client.transport import TCPTransport
-		from riemann_client.client import QueuedClient
-
-		with QueuedClient(TCPTransport(self.host, int(self.port))) as client:
+		if self.status:
 			for k, v in self.status.iteritems():
-				client.event(service = k, state = v, metric_f = new_times_running[k], host = self.syshost)
-			client.flush()
+				print(self.client.send(dict(service = "kensho.test.proc." + k, state = v, metric = new_times_running[k], host = self.syshost, ttl = 600)))
 
-		threading.Timer(self.interval, self.main).start()
+		#print(self.data)
+		#print(self.status)
+		threading.Timer(int(self.interval), self.collect_and_emit).start()
 
 @click.command()
 @click.option('--host' ,'-h', default ='127.0.0.1', help = 'The riemann host')
 @click.option('--timeout', '-m', default = '5', help = 'The timeout time')
 @click.option('--port', '-p', default = '5555', help = 'The riemann port')
-@click.option('--event-host','-e', default = '', help = 'Event hostname')
 @click.option('--interval', '-i', default ='5', help = 'Seconds between updates')
 @click.option('--procs', default = '', help = 'Filters services')
 @click.option('--directory', '-d', default = '/etc/service/', help = 'Directory')
